0a612256185b42eef34cf7158592039864b08d25
Michael Cheng <cheng.c.mike@gmail.com>
2015-05-17 19:24:12 -0500

    edit

--- server/config/socketioconfigexample.js
+++ server/config/socketioconfigexample.js
@@ -1,10 +1,10 @@
 var Twit = require('twit');
 
 var T = new Twit({
-  consumer_key: 'LzrzGCydhWiwdBBleA5TfxLOO', 
-  consumer_secret: 'Qb3Io9LVPAyW0FOzdQUwqQ2ymNv7cSGrEa3fw5OcH9qKdCOrOB', 
-  access_token: '19341319-ZafhlXVvwKtwddQh8vmU6b6XylgxTKsd0cwOBwJuD', 
-  access_token_secret: 'FG1DWyme86ZfGRJTlF3x6oBsu7FBQCmKLCqMuSDs7ZUQB'
+  consumer_key: 'ENTER YOURS HERE', 
+  consumer_secret: 'ENTER YOURS HERE', 
+  access_token: 'ENTER YOURS HERE', 
+  access_token_secret: 'ENTER YOURS HERE'
 });
 
 module.exports = T;

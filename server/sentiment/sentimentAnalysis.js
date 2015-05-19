cd818dc3de6bd8fbad85444117c7a03351b38596
Michael Cheng <cheng.c.mike@gmail.com>
2015-05-19 00:23:06 -0500

    (feat) improve sentiment analysis results

--- server/sentiment/sentimentAnalysis.js
+++ server/sentiment/sentimentAnalysis.js
@@ -4,23 +4,45 @@ var sentimentNegative = sentimentWords.sentimentNegative;
 
 var sentimentAnalysis = function(data) {
   var results = {};
-  results.finalScore = 0;
+  var totalSentimentScore = 0;
+  var totalSentiment;
 
   data.forEach(function(text, i) {
     results[i + 1] = {};
     results[i + 1].text = text;
     results[i + 1].positiveWords = [];
     results[i + 1].negativeWords = [];
+    results[i + 1].score = 0;
     text.split(' ').forEach(function(word) {
       if (sentimentPositive[word]) {
         results[i + 1].positiveWords.push([word, sentimentPositive[word]]);
-        results.finalScore += sentimentPositive[word];
+        results[i + 1].score++;
       } else if (sentimentNegative[word]) {
         results[i + 1].negativeWords.push([word, sentimentNegative[word]]);
-        results.finalScore += sentimentNegative[word];
+        results[i + 1].score--;
+      }
+      if (results[i + 1].score > 0) {
+        results[i + 1].sentiment = {score: 1, sentiment: 'positive'};
+      } else if (results[i + 1].score < 0) {
+        results[i + 1].sentiment = {score: -1, sentiment: 'negative'};
+      } else {
+        results[i + 1].sentiment = {score: 0, sentiment: 'neutral'};
       }
     });
   });
+
+  for (var key in results) {
+    totalSentimentScore += results[key].sentiment.score;
+    if (totalSentimentScore > 0) {
+      totalSentiment = 'positive';
+    } else if (totalSentimentScore < 0) {
+      totalSentiment = 'negative';
+    } else {
+      totalSentiment = 'neutral';
+    }
+
+    results.totalSentiment = {score: totalSentimentScore, sentiment: totalSentiment};
+  }
 
   return results;
 };

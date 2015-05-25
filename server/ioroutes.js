d3e3b8d25404d148b7023bce2fd3ab2e958e78ab
Michael Cheng <cheng.c.mike@gmail.com>
2015-05-24 21:03:52 -0500

    (doc) add comments to and cleanup ioroutes

--- server/ioroutes.js
+++ server/ioroutes.js
@@ -12,61 +12,34 @@ module.exports = function(io, T) {
 
   io.on('connection', function(socket) {
 
-    
-    
-    // Receives a constant sample stream of twitter statuses
-    socket.on('twitter stream sample', function(num) {
+    // Gets top 10 trending topics.
+    // This is requested on main.html page load and used for the sentiment display section
+    socket.on('twitter rest trending', function() {
 
-      // Counter used to track how many tweets have been received
-      var count = 0;
-      var target = num || 20;
-      var tweetsSentimentArray = [];
-      var twemojiTextArray = [];
+      T.get('trends/place', {id: 23424977}, function(err, data) {
 
-      var tweetsArray = [];
-      
-      var stream = T.stream('statuses/sample');
-      stream.on('tweet', function(tweet) {
-
-        if (tweet.lang === 'en') {
-          io.emit('twitter stream sample', tweet);
-          count++;
-
-          tweetsArray.push(tweet);
-        }
-
-        if (count === target) {
-          stream.stop();
-
-          var allLayersResults = allLayersAnalysis.tweetsArray(tweetsArray);
-          
-          io.emit('all layers', allLayersResults);
-        }
+        socket.emit('twitter rest trending', data);
       });
     });
 
-    socket.on('twitter stream filter', function(num, topics) {
+    // Gets tweets for a search query
+    // This is used for the sentiment display section
+    socket.on('twitter rest search', function(query, result_type, count) {
 
-      var count = 0;
-      var target = num || 20;
-      var tweetsArray = [];
+      var params = {
+        q: query,
+        // Up to 100
+        count: count,
+        // "recent", "mixed", or "popular"
+        result_type: result_type
+      };
 
-      var stream = T.stream('statuses/filter', {track: topics, language: 'en'});
+      T.get('search/tweets', params, function(err, data) {
 
-      stream.on('tweet', function(tweet) {
-        io.emit('twitter stream filter', tweet);
-        
-        count++;
-        
-        tweetsArray.push(tweet);
-          
-        if (count === target) {
-          stream.stop();
-
-          var allLayersResults = allLayersAnalysis.tweetsArray(tweetsArray);
-          
-          io.emit('all layers', allLayersResults);
-        }
+        // Add layer analyses to each tweet
+        var allLayersResults = allLayersAnalysis.tweetsArray(data.statuses);
+                  
+        io.emit('all layers', allLayersResults);
       });
     });
 
@@ -94,43 +67,8 @@ module.exports = function(io, T) {
       }
     });
 
-    socket.on('twitter rest user timeline', function(screen_name, count) {
-      T.get('statuses/user_timeline', {screen_name: screen_name, count: count}, function(err, data) {
-        socket.emit('twitter rest user timeline', data);
-
-        var allLayersResults = allLayersAnalysis.tweetsArray(data);
-        
-        io.emit('all layers', allLayersResults);
-
-      });
-    });
-
-    socket.on('twitter rest search', function(query, result_type, count) {
-
-      T.get('search/tweets', {q: query, count: count, result_type: result_type}, function(err, data) {
-        // socket.emit('twitter rest search', data);
-        var allLayersResults = allLayersAnalysis.tweetsArray(data.statuses);
-                  
-        io.emit('all layers', allLayersResults);
-      });
-    });
-
-    socket.on('home timeline', function() {
-      T.get('statuses/home_timeline', {count: 50}, function(err, data) {
-        console.log(data);
-        var allLayersResults = allLayersAnalysis.tweetsArray(data);
-                  
-        io.emit('all layers', allLayersResults);
-
-        // socket.emit('home timeline', data);
-      });
-    });
-
-    socket.on('twitter rest trending', function() {
-      T.get('trends/place', {id: 23424977}, function(err, data) {
-        socket.emit('twitter rest trending', data);
-      });
-    });
+    // ===== THIS IS USED TO DOWNLOAD TWEETS TO THE DATABASE ====== //
+    // Not relevant to production
 
     var streamDownload;
 

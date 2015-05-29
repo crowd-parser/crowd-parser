f316b9c3d1d6337bb618ebe46c358d886181bcd4
Michael Cheng <cheng.c.mike@gmail.com>
2015-05-29 15:49:10 -0500

    (feat) add stemmer module from NaturalNode

---
+++ server/sentiment/negationLayer/negationList.js
@@ -0,0 +1,12 @@
+var negationWords = {
+
+  'not': true,
+  'don\'t': true,
+  'isn\'t': true,
+  'wasn\'t': true,
+  'aren\'t': true,
+  'can\'t': true,
+  'won\'t': true,
+};
+
+module.exports = negationWords;

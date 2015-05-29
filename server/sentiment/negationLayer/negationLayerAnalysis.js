de6df8236b075ebba25171dcf12f41900f55491e
Michael Cheng <cheng.c.mike@gmail.com>
2015-05-29 16:41:40 -0500

    (feat) start structure for negation layer

--- server/sentiment/negationLayer/negationLayerAnalysis.js
+++ server/sentiment/negationLayer/negationLayerAnalysis.js
@@ -1,35 +1,43 @@
 var negationWords = require('./negationList');
 
+var baseWords = require('../baseWordsLayer/baseWordsList');
+
 module.exports = function(string) {
 
   // Initialize results object with what we want in the end
   var results = {
-    positiveWords: [],
-    negativeWords: [],
+    negationWords: [],
+    negatedPositives: [],
     score: 0
   };
 
   // Perform the base words layer analysis on the string
   string.split(' ').forEach(function(word, i) {
 
-    // Make a lowercase copy of each word to compare against the base words library
-    var lowerCaseWord = word.toLowerCase();
+    
 
-    // If the copied word matches a positive word in the library, add the original word to the positive words array
-    if (sentimentPositive[lowerCaseWord]) {
-      results.positiveWords.push([word, i]);
+    // Make a lowercase copy of each word to compare against the negation words library
+    var lowerCaseWord = word.toLowerCase().replace(/[\.\!]/g, '');
 
-      // Increment the final score of the string
-      results.score++;
+    if (baseWords.sentimentPositive[lowerCaseWord] || negationWords[lowerCaseWord]) {
+      
+      if (baseWords.sentimentNegative[string.split(' ')[i - 1]]) {
+        results.negatedPositives.push([word, i]);
+        results.score--;
+      }
+    
+    // If the copied word matches a word in the negation library, add the word to the negation words array
+    } else if (negationWords[lowerCaseWord]) {
+      results.negationWords.push([word, i]);
 
-    // If the copied word matches a negative word in the library, add the original word to the negative words array  
-    } else if (sentimentNegative[lowerCaseWord]) {
-      results.negativeWords.push([word, i]);
-
-      // Decrement the final score of the string
+      // Decrement the final score of the string, since negation words are generally intrinsically negative
       results.score--;
     }
+
+    
   });
 
   return results;
 };
+
+console.log(module.exports('not cool'));

ce865b3b17c2ff8f026bcf8ba5865e7b01205556
Michael Cheng <cheng.c.mike@gmail.com>
2015-05-30 17:40:46 -0500

    (feat) emojianalysis now takes tweet objects with unicode chars

--- server/sentiment/emoticonLayer/emoticonLayerAnalysis.js
+++ server/sentiment/emoticonLayer/emoticonLayerAnalysis.js
@@ -96,7 +96,7 @@ module.exports.tweetObject = function(tweetObject) {
     score: 0
   };
 
-  var string = tweetObject.text;
+  var string = emojiConverter.restoreEmojisInTweet(tweetObject.text);
 
   // Create an array of the emojis in the string
   var emojis = string.match(emojiRegex());
@@ -114,7 +114,8 @@ module.exports.tweetObject = function(tweetObject) {
 
       // If emoji is in the positive emoticons list, add to positive words array
       if (itemCode in em.positive) {
-        item = '<%-' + emojiConverter.toCodePoint(item) + '%>';
+        item = emojiConverter.convertEmojisInTweet(item);
+
         results.positiveWords.push(item);
 
         // Increment the final score of the string
@@ -122,7 +123,8 @@ module.exports.tweetObject = function(tweetObject) {
 
       // If emoji is in the negative emoticons list, add to negative words array
       } else if (toCodePoint(item) in em.negative) {
-        item = '<%-' + emojiConverter.toCodePoint(item) + '%>';
+        item = emojiConverter.convertEmojisInTweet(item);
+
         results.negativeWords.push(item);
 
         // Decrement the final score of the string
@@ -138,3 +140,7 @@ module.exports.tweetObject = function(tweetObject) {
 
   return results;
 };
+
+// @LanaeBeau_TY <%-1f625%><%-1f625%><%-1f625%> suck sad happy frowing you are awesome obama yay
+
+// console.log(module.exports.tweetObject({"created_at":"Wed May 20 23:13:04 +0000 2015","id":601163762242981900,"id_str":"601163762242981888","text":"@LanaeBeau_TY <%-1f625%><%-1f625%><%-1f625%> suck sad happy frowing you are awesome obama yay","source":"<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>","truncated":false,"in_reply_to_status_id":601160439414857700,"in_reply_to_status_id_str":"601160439414857728","in_reply_to_user_id":277764780,"in_reply_to_user_id_str":"277764780","in_reply_to_screen_name":"LanaeBeau_TY","user":{"id":3252488177,"id_str":"3252488177","name":"●○●○♡","screen_name":"2411Clark","location":"","url":null,"description":"I'm dope just follow .","protected":false,"verified":false,"followers_count":7,"friends_count":29,"listed_count":0,"favourites_count":11,"statuses_count":40,"created_at":"Wed May 13 19:04:37 +0000 2015","utc_offset":null,"time_zone":null,"geo_enabled":false,"lang":"en","contributors_enabled":false,"is_translator":false,"profile_background_color":"C0DEED","profile_background_image_url":"http://abs.twimg.com/images/themes/theme1/bg.png","profile_background_image_url_https":"https://abs.twimg.com/images/themes/theme1/bg.png","profile_background_tile":false,"profile_link_color":"0084B4","profile_sidebar_border_color":"C0DEED","profile_sidebar_fill_color":"DDEEF6","profile_text_color":"333333","profile_use_background_image":true,"profile_image_url":"http://pbs.twimg.com/profile_images/600318202212519937/4Qidlfxo_normal.jpg","profile_image_url_https":"https://pbs.twimg.com/profile_images/600318202212519937/4Qidlfxo_normal.jpg","profile_banner_url":"https://pbs.twimg.com/profile_banners/3252488177/1431961882","default_profile":true,"default_profile_image":false,"following":null,"follow_request_sent":null,"notifications":null},"geo":null,"coordinates":null,"place":null,"contributors":null,"retweet_count":0,"favorite_count":0,"entities":{"hashtags":[],"trends":[],"urls":[],"user_mentions":[{"screen_name":"LanaeBeau_TY","name":"tyisha.","id":277764780,"id_str":"277764780","indices":[0,13]}],"symbols":[]},"favorited":false,"retweeted":false,"possibly_sensitive":false,"filter_level":"low","lang":"en","timestamp_ms":"1432163584658"}));

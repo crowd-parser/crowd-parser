f0ae255a23ff5631d63bad98c5cf7742c504abf8
Christina Holland <hsubox@gmail.com>
2015-05-30 11:57:19 -0700

    Minimal negation layer set up.

--- server/sentiment/negationLayer/negationList.js
+++ server/sentiment/negationLayer/negationList.js
@@ -1,12 +1,24 @@
 var negationWords = {
-
-  'not': true,
-  'don\'t': true,
-  'isn\'t': true,
-  'wasn\'t': true,
-  'aren\'t': true,
-  'can\'t': true,
-  'won\'t': true,
+  'not only': "~PSEU~",
+  'don\'t see': "~PSEU~",
+  'don\'t know': "~PSEU~",
+  'don\'t understand': "~PSEU~",
+  'not': "~PREN~",
+  // 'n\'t': "~PREN~",
+  'don\'t': "~PREN~",
+  'isn\'t': "~PREN~",
+  'wasn\'t': "~PREN~",
+  'aren\'t': "~PREN~",
+  'can\'t': "~PREN~",
+  'won\'t': "~PREN~",
+  'unlikely': "~POST~",
+  'but': "~CONJ~",
+  'however': "~CONJ~",
+  'yet': "~CONJ~",
+  'though': "~CONJ~",
+  'although': "~CONJ~",
+  'still': "~CONJ~",
+  'except': "~CONJ~",
 };
 
 module.exports = negationWords;

f91aa9b66eb471359f76dba15e6d72a7cd7b72fa
Michael Cheng <cheng.c.mike@gmail.com>
2015-06-02 23:26:39 -0500

    (cleanup) remove login from routes

--- server/auth/index.js
+++ server/auth/index.js
@@ -3,7 +3,6 @@
 var express = require('express');
 var router = express.Router();
 
-router.use('/login', require('./login'));
 router.use('/adminlogin', require('./adminlogin'));
 router.use('/checkAuth', require('./checkAuth'));
 

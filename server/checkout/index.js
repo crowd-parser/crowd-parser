13722006e74dfa15d52ad9bd840fb6af0a78bf62
Michael Cheng <cheng.c.mike@gmail.com>
2015-06-05 21:14:25 -0500

    (fix) fix another issue with stripe

--- server/checkout/index.js
+++ server/checkout/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 
 // Set your secret key: remember to change this to your live secret key in production
 // See your keys here https://dashboard.stripe.com/account/apikeys
-var stripe = require("stripe")("pk_live_x1auH0us8kSI1LvxZ1jcZGc6");
+var stripe = require("stripe")("sk_live_CFGeujb4KYZXrHWeYZo8ZyK2");
 
 router.post('/purchase', function(req, res, next) {
 
